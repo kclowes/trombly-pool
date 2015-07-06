4ba4005b2544a398f3da490e1e7e1362de0ec36e
Keri Clowes <kclowes@quickleft.com>
2015-07-05 21:09:30 -0600

    Style Sign in/Sign up views

--- Gemfile
+++ Gemfile
@@ -12,9 +12,13 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise', '~> 3.5.1'
 gem 'pundit', '~> 1.0.1'
+gem 'bootstrap-sass', '~> 3.3.5'
 
 group :development, :test do
   gem 'byebug'
   gem 'web-console', '~> 2.0'
 end
 
+group :production do
+  gem 'rails_12factor', '~> 0.0.3'
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
 
   after_action :verify_authorized, unless: :devise_controller?, :except => :index
   after_action :verify_policy_scoped, unless: :devise_controller?, :only => :index
+
+  private
+
+  def after_sign_in_path_for(user)
+    games_path
+  end
+
+  def after_register_path_for(user)
+    games_path
+  end
 end
